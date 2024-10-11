18a2f3de09d68bb6744f6a171126bf6de8c4514e
Ferra <martin.ferrarese@gmail.com>
2024-10-11 16:20:18 -0300

    feat: limpieza de pantalla principal

--- src/app/page.spec.ts
+++ src/app/page.spec.ts
@@ -1,12 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import Home from './page';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, afterEach } from 'vitest';
 
 describe('Página inicial', () => {
-  it('Smoke test', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("debe mostrar el título 'Comidita' y el footer", () => {
     render(Home());
-    expect(
-      screen.getByText('Save and see your changes instantly.')
-    ).toBeTruthy();
+
+    expect(screen.getByTestId('titulo')).toBeTruthy();
+    expect(screen.getByTestId('titulo').innerHTML).toBe('COMIDITA');
+    expect(screen.getByTestId('texto-footer').innerHTML).toBe(
+      'Hecho por Martín Ferrarese'
+    );
+  });
+
+  it.skip('debe mostrar una lista de comidas', () => {
+    const listaDeComidas = [
+      {
+        comida: 'Milanesas con puré',
+        ingredientes: [
+          'milanesa de carne',
+          'pan rallado',
+          'panko',
+          'huevo',
+          'ajo',
+          'papa',
+          'manteca',
+          'leche',
+          'sal',
+        ],
+      },
+      {
+        comida: 'Fideos con crema',
+        ingredientes: ['fideos', 'crema', 'pimienta', 'sal'],
+      },
+    ];
+
+    render(Home());
+
+    expect(screen.getByTestId('lista-de-comidas')).toBeTruthy();
   });
 });
